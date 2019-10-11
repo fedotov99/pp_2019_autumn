c9a5ad83a593b7377e0997dcdea100c137a7e57c
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-11 18:06:55 +0300

    header changed

--- modules/task_1/fedotov_v_mins_in_matrix_rows/mins_in_matrix_rows.h
+++ modules/task_1/fedotov_v_mins_in_matrix_rows/mins_in_matrix_rows.h
@@ -15,13 +15,16 @@ extern int minsByParallel[];
 
 std::vector<int> getRandomVector();
 void getRandomMatrix();
-void printVector(std::vector<int> vec);
-void printMatrix();
 
 int getSequentialMinInRow(std::vector<int> row); // in definite row, not in all matrix
 int getParallelMinInRow(std::vector<int> row); // in definite row, not in all matrix
 
 void getSequentialMinsInMatrix(); // mins of each row of matrix will be written in array mins[ROWS_COUNT]
 void getParallelMinsInMatrix(); // mins of each row of matrix will be written in array mins[ROWS_COUNT]
+
+void printVector(std::vector<int> vec);
+void printMatrix();
+void printMins();
+void printMinsByParallel();
 
 #endif  // MODULES_MINS_IN_MATRIX_ROWS_H_
