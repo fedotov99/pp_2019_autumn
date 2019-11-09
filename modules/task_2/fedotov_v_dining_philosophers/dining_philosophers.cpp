6ff84f15416022b544bc707b603d051dc0d9787c
Vlad Fedotov <fedotovvlad@list.ru>
2019-11-09 18:42:03 +0300

    Initial commit

---
+++ modules/task_2/fedotov_v_dining_philosophers/dining_philosophers.cpp
@@ -0,0 +1,9 @@
+#include <mpi.h>
+#include <vector>
+#include <string>
+#include <random>
+#include <algorithm>
+#include <ctime>
+#include <iostream>
+#include "../../../modules/task_2/fedotov_v_dining_philosophers/dining_philosophers.h"
+
