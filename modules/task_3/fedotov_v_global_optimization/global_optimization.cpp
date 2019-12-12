83b59964941eeceabe5e28445e9c741c5ff22066
Vlad Fedotov <fedotovvlad@list.ru>
2019-12-12 19:39:17 +0300

    Initial

---
+++ modules/task_3/fedotov_v_global_optimization/global_optimization.cpp
@@ -0,0 +1,11 @@
+// Copyright 2019 Fedotov Vlad
+#include <mpi.h>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <list>
+#include <algorithm>
+#include <iostream>
+#include "../../../modules/task_3/fedotov_v_global_optimization/global_optimization.h"
+
---
+++ modules/task_3/fedotov_v_global_optimization/main.cpp
@@ -0,0 +1,30 @@
+// Copyright 2019 Fedotov Vlad
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include <iostream>
+#include "./global_optimization.h"
+
+// TEST(Parallel_Operations_MPI, Test_Philosophers_Ended_Their_Job) {
+//     int rank, size;
+//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
+//     }
+// }
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+
+    return RUN_ALL_TESTS();
+}
